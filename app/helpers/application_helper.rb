9afcd0247bcc3db9fff5743c6590fc833a865541
David A McClain <david@uhhuhyeah.com>
2012-07-13 21:12:42 -0700

    Date formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  include ActiveSupport::Inflector
+
+  def date_format(time)
+    parts = ['Thursday']
+    parts << ordinalize(time.strftime('%d').to_i)
+    parts << time.strftime('%B')
+    parts.join(' ')
+  end
+
 end
