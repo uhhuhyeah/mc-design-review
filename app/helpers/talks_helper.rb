09385be14aa9bcc2c50787b55e5255a266d7b7fe
David A McClain <david@uhhuhyeah.com>
2012-07-22 09:53:38 -0700

    Post talk to rubydev

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -15,4 +15,34 @@ module TalksHelper
     "#{count || 0} " + (count == 1 ? singular : (plural || singular.pluralize))
   end
 
+  def post_to_ruby_dev(talk)
+    presenters = talk.co_presenter ? "#{talk.presenter.name} and #{talk.co_presenter.name}" : talk.presenter.name
+    to = Rails.env.production? ? 'rubydev@modcloth.com' : 'test@example.com'  
+    subject = "#{truncate(talk.title, length: 30)} talk scheduled for #{date_format(talk.date)}"
+    body = <<-DOC
+      On #{date_format(talk.date)} #{presenters} will be giving a talk.
+      %0D%0A
+      %0D%0A
+      Talk title: #{talk.title}
+      "#{talk.description}"
+      %0D%0A
+      %0D%0A
+      Who should attend?
+      #{talk.attend}
+      %0D%0A
+      %0D%0A
+      What can we expect to get from this talk?
+      #{talk.expect}
+      %0D%0A
+      %0D%0A
+      What should we do to prepare for this talk?
+      #{talk.prepare}
+      %0D%0A
+      %0D%0A
+      Link: #{talk_url(talk)}
+    DOC
+
+    "mailto:#{to}?subject=#{subject}&body=#{body}"
+  end
+
 end
