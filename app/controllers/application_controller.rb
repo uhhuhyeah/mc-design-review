5449379c521d84b466c7e592882cadb48f818769
David A McClain <david@uhhuhyeah.com>
2012-09-12 14:28:56 -0700

    Chair display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+  
+  helper_method :chairperson
+  
+  def chairperson
+    @chairperson ||= Chair.current
+  end
 end
